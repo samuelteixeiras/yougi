8a19db76b307fa827aa8ca2458e711a4bfac5082
Hildeberto Mendonca <me@hildeberto.com>
2013-04-28 18:41:54 +0200

    Issue #66: Dependencies added to Maven and a simple test created.

---
+++ yougi-ejb/src/test/java/org/cejug/yougi/test/business/LocationBeanTest.java
@@ -0,0 +1,43 @@
+package org.cejug.yougi.test.business;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+
+/**
+ *
+ * @author htmfilho
+ */
+@RunWith(Arquillian.class)
+public class LocationBeanTest {
+    
+    public LocationBeanTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {
+        //Assert.fail("Not yet implemented");
+    }
+}
